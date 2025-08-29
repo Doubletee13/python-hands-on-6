b92685f9565fc2bab8934f9dd7505f923bce91eb
Doubletee13 <akinlade_temitope0506@yahoo.com>
2025-08-29 16:12:02 +0100

    task3

--- task3.py
+++ task3.py
@@ -18,3 +18,23 @@ print(calculate_grade(60))   # D
 print(calculate_grade(70))   # C
 print(calculate_grade(89))   # B
 """
+
+
+def calculate_grade(score):
+	if score >= 90 and score <= 100:
+		print("A")
+	elif score >= 80 and score <= 89:
+		print("B")
+	elif score >= 70 and score <= 79:
+		print("C")
+	elif score >= 60 and score <= 69:
+		print("D")
+	elif score >= 0 and score < 60:
+		print("F")
+	else:
+		print("Invalid score")
+	
+
+score = int(input("enter a score: "))
+
+calculate_grade(score)
